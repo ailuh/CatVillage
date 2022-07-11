33c81fe9430b04456bae5db9cb7f1584ce0c20cd
Anton Ilyukhin <aii@devkeeper.ru>
2022-07-11 21:12:12 +0300

    started game concept with free sprites

---
+++ Assets/Scripts/Components/BuildUIActivator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildUIActivator : MonoBehaviour
+{
+    [SerializeField] UIService _uiService;
+
+    private void OnTriggerEnter2D(Collider2D collision)  
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("It Works");
+            _uiService.BuildingUIView(true, transform.position);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("It Works");
+            _uiService.BuildingUIView(false, transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Services/UIService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIService : MonoBehaviour
+{
+    public List<Button> BuildButtons = new List<Button>();
+    public GameObject BuildingView;
+
+    private void Start()
+    {
+        BuildUIInitialize();
+    }
+
+    void BuildUIInitialize()
+    {
+        BuildingView.SetActive(false);
+    }
+
+    public void BuildingUIView(bool isOn, Vector3 position)
+    {
+        BuildingView.SetActive(isOn);
+        BuildingView.transform.position = position;
+    }
+}
